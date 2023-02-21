43e7cdc0c39635e24ada0b7d739acd22365e6dec
Gildas Quéméner <gildas.quemener@gmail.com>
2023-02-21 13:00:47 +0100

    Fix warning

--- src/api.js
+++ src/api.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import {loadRestaurants} from './store/restaurants/actions';
 
 const client = axios.create({
   baseURL: 'https://api.outsidein.dev/fK671Jzf3s96951pnQ4ijuvSnp6Yr55e',
