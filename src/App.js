223de39f0d48aa68caf99e5e2a05ff74edb40eaf
Gildas Quéméner <gildas.quemener@gmail.com>
2023-02-21 10:36:12 +0100

    Style the UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import {Provider} from 'react-redux';
 import store from './store';
 import RestaurantScreen from './components/RestaurantScreen';
+import {createTheme} from '@mui/material/styles';
+import {blueGrey} from '@mui/material/colors';
+import {ThemeProvider} from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+
+const theme = createTheme({
+  palette: {
+    primary: blueGrey,
+  },
+});
 
 export default function App() {
   return (
     <Provider store={store}>
-      <RestaurantScreen />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6">Opinion Ate</Typography>
+          </Toolbar>
+        </AppBar>
+        <Container>
+          <RestaurantScreen />
+        </Container>
+      </ThemeProvider>
     </Provider>
   );
 }
