1c3e13ad2156b0897b99e71b2a630e64213e6d26
Gildas Quéméner <gildas.quemener@gmail.com>
2023-02-21 12:57:11 +0100

    Configure redux debugger

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,12 @@
-import {applyMiddleware, createStore} from 'redux';
+import {applyMiddleware, compose, createStore} from 'redux';
 import thunk from 'redux-thunk';
 import api from '../api';
 import rootReducer from './reducers';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
-  applyMiddleware(thunk.withExtraArgument(api)),
+  composeEnhancers(applyMiddleware(thunk.withExtraArgument(api))),
 );
 
 export default store;
